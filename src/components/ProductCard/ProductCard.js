23bc60dd12cba009e6b631783dc388fcc9dfcb3f
immanual19 <immanuelsarker1@gmail.com>
2021-04-02 01:55:06 +0600

    reDesigned

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -4,7 +4,8 @@ import './ProductCard.css';
 import { Link, useHistory } from 'react-router-dom';
 const ProductCard = (props) => {
     
-    const {_id,name,price,imageURL}=props.product;
+    const {_id,name,price,weight,imageURL}=props.product;
+    console.log(props.product.weight);
     const history=useHistory();
     const handleClick=(id)=>{
         const url=`/checkoutproduct/${id}`;
@@ -12,17 +13,17 @@ const ProductCard = (props) => {
     }
     return (
         <div>
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '18rem'}}>
         <Card.Img variant="top" src={imageURL} />
         <Card.Body>
-          <Card.Title style={{fontSize:'15px'}}>{name}</Card.Title>
-          
-          <div style={{marginTop:'20px'}} className="price-and-button d-flex justify-content-between align-items-center">
-                <h3 style={{display:'inline-block'}} className="price">${price}</h3>
+          <Card.Title style={{fontSize:'12px'}}>{name} - {weight}</Card.Title>
+        </Card.Body>
+        <Card.Footer>
+        <div style={{marginTop:'20px'}} className="d-flex justify-content-between align-items-center">
+                <h6 style={{display:'inline-block'}} className="price">${price}</h6>
                <Link style={{display:'inline-block'}} to={`/checkoutproduct/${_id}`}><Button onClick={()=>handleClick(_id)} className="button" variant="primary">Buy Now</Button></Link>
           </div>
-          
-        </Card.Body>
+        </Card.Footer>
       </Card>
         </div>
     );
